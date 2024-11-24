c45ed2184170009aaa81cc77331669d8411c1879
Khairunnisa Johan Thani <khai.jt@outlook.com>
2024-11-24 13:39:26 +0000

    Save progress: 1290.1

--- student_info.sh
+++ student_info.sh
@@ -6,4 +6,5 @@ echo -e "\n~~ My Computer Science Students ~~\n"
 
 PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
 
-echo "First name, last name, and GPA of students with a 4.0 GPA:"
+echo -e "\nFirst name, last name, and GPA of students with a 4.0 GPA:"
+
